a4164e1b4f27dc6f5833fca85b379a9d5a71b3f8
unknown <cyriaque.devisme@epita.fr>
2021-04-27 21:25:37 +0200

    IIIIH

--- Assets/Scripts/ZoneChange.cs
+++ Assets/Scripts/ZoneChange.cs
@@ -5,32 +5,16 @@ using UnityEngine;
 public class ZoneChange : MonoBehaviour
 {
     public Third_Person_Movement player;
-    public bool mmmh = false;
     public GameObject Saphyria;
     public GameObject Nuage;
 
-    public GameObject walou;
-    public GameObject wala;
-
     IEnumerator OnTriggerEnter(Collider other)
     {
-        if (walou.gameObject.tag == "Player" && !mmmh)
+        if (other.gameObject.tag == "Player")
         {
-            mmmh = true;
             Saphyria.SetActive(true);
             yield return new WaitForSeconds(2);
             Saphyria.SetActive(false);
-        }
-    }
-
-    IEnumerator OnTriggerEnter2(Collider other)
-    {
-        if (wala.gameObject.tag == "Player" && mmmh)
-        {
-            mmmh = false;
-            Nuage.SetActive(true);
-            yield return new WaitForSeconds(2);
-            Nuage.SetActive(false);
         }
     }
 }
---
+++ Assets/Scripts/ZoneChange2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneChange2 : MonoBehaviour
+{
+    public Third_Person_Movement player;
+    public GameObject Saphyria;
+    public GameObject Nuage;
+
+    IEnumerator OnTriggerEnter2(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Nuage.SetActive(true);
+            yield return new WaitForSeconds(2);
+            Nuage.SetActive(false);
+        }
+    }
+}
