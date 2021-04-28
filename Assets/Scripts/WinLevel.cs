0b22a7ccc54a1a0bbe9c60b0b61a955ff5a735f9
unknown <cyriaque.devisme@epita.fr>
2021-04-29 00:24:51 +0200

    #branl team

--- Assets/Scripts/WinLevel.cs
+++ Assets/Scripts/WinLevel.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class WinLevel : MonoBehaviour
 {
     public Third_Person_Movement player;
+    public bool level;
+    public GameObject WinMenu;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player" && player.hasObject)
         {
             gameObject.SetActive(false);
+            level = true;
+        }
+        if(level == true)
+        {
+            WinMenu.SetActive(true);
+            Time.timeScale= 0f;
         }
     }
 }
