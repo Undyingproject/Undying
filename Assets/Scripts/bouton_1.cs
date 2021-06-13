441cc98033a999d02ab5703ceccf2b5a00dffad2
unknown <cyriaque.devisme@epita.fr>
2021-06-13 22:57:51 +0200

    wewewe

---
+++ Assets/Scripts/bouton_1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bouton_1 : MonoBehaviour
+{
+    public Third_Person_Movement player;
+    public GameObject Object;
+    public GameObject Object2;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Object.SetActive(false);
+            Object2.SetActive(false);
+        }
+    }
+}
