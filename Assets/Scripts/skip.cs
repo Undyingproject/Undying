79513294832668419cb81285215be6e6c82ea852
unknown <cyriaque.devisme@epita.fr>
2021-06-19 23:23:50 +0200

    AH-AH-AH

---
+++ Assets/Scripts/skip.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skip : MonoBehaviour
+{
+    public GameObject Image;
+    public GameObject camera;
+    public GameObject diagman;
+    public GameObject diagman2;
+    public void SkipText()
+    {
+        Destroy(Image);
+        Destroy(diagman);
+        Destroy(diagman2);
+        camera.SetActive(true);
+    }
+}
