752889c44b4b3bdf7aa1f5fe80dc5d64ff52f4c7
unknown <cyriaque.devisme@epita.fr>
2021-06-19 20:46:08 +0200

    RRRRRRAAAAAAAAAAAAAAHHHHHHHH PUT-THEU J'AI DIT PUUT-HEU

---
+++ Assets/Scripts/breakbyedynamite.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class breakbyedynamite : MonoBehaviour
+{
+    public Third_Person_Movement player;
+    public bool level;
+    public GameObject mur;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && player.hasObject)
+        {
+            mur.SetActive(false);
+        }
+    }
+}
