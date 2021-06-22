62058ecc6e72a609b6d56e08a1aa1be8a9e69e2d
unknown <cyriaque.devisme@epita.fr>
2021-06-22 18:56:29 +0200

    oui

---
+++ Assets/Scripts/bouton_2_1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bouton_2_1 : MonoBehaviour
+{
+    public bool test1 = false;
+    public bool test2 = false;
+
+    public Third_Person_Movement player;
+    public GameObject Object;
+    public GameObject Object2;
+    public GameObject Object3;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Button_1")
+        {
+            Object.SetActive(false);
+            test1 = true;
+        }
+        if (other.gameObject.tag == "Button_2")
+        {
+            Object2.SetActive(false);
+            test2 = true;
+        }
+    }
+
+    void Update()
+    {
+        if (test1 && test2)
+        {
+            Object3.SetActive(true);
+        }
+    }
+}
